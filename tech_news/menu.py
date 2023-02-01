d0bbdd04fc0998ea270149df395adca573e5cdd6
Eduardomak <eduardomak92@gmail.com>
2023-01-31 22:13:29 -0300

    Refac: correcting lint

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -18,22 +18,19 @@ options = [
 
 def switch(string):
 
+    dict_switch = {
+        "0": "Digite quantas notícias serão buscadas:",
+        "1": "Digite o título:",
+        "2": "Digite a data no formato aaaa-mm-dd:",
+        "3": "Digite a tag:",
+        "4": "Digite a categoria:",
+    }
+
     if not string.isnumeric() or int(string) < 0 or int(string) > 7:
         print(ValueError("Opção inválida"), file=sys.stderr)
-    elif int(string) == 0:
-        print("Digite quantas notícias serão buscadas:")
 
-    elif int(string) == 1:
-        print("Digite o título:")
-
-    elif int(string) == 2:
-        print("Digite a data no formato aaaa-mm-dd:")
-
-    elif int(string) == 3:
-        print("Digite a tag:")
-
-    elif int(string) == 4:
-        print("Digite a categoria:")
+    for _ in dict_switch:
+        print(dict_switch[_])
 
 
 def analyzer_menu():
